757396738493a6c18b9283d6041cb4f3c2f327e5
Mieszko <mirgosmieszko@gmail.com>
2021-04-14 21:25:10 +0200

    ok up to 1.3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-from fastapi import FastAPI
+import hashlib
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,12 +14,30 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}", response_model=HelloResp)
-async def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
+@app.get("/method")
+async def getres():
+    return {"method":"GET"}
+
+@app.post("/method", status_code=201)
+async def getres():
+    return {"method":"POST"}
+
+@app.post("/auth")
+async def hashver(password:str, password_hash:str):
+    if (not password)or(not password_hash):
+        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+    print(hashlib.sha512(password.replace("\n","").strip().encode('utf-8')))
+    if hashlib.sha512(password.replace("\n","").strip().encode('utf-8')).hexdigest()==password_hash:
+        
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+# @app.get("/hello/{name}", response_model=HelloResp)
+# async def read_item(name: str):
+#     return HelloResp(msg=f"Hello {name}")
 
 
-@app.get('/counter')
-def counter():
-    app.counter += 1
-    return app.counter
+# @app.get('/counter')
+# def counter():
+#     app.counter += 1
+#     return app.counter
