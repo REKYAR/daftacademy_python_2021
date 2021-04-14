e5ec2e09890f9cf91be0df4d1ae792816832cd86
Mieszko <mirgosmieszko@gmail.com>
2021-04-14 19:44:53 +0200

    after first meeting

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 
+import pytest
+
 from main import app #w sensie plik main
 
 client = TestClient(app)
@@ -9,3 +11,20 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == "Hello {name}"
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
